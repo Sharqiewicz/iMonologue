630d907725e4f1c025d2a41cfa6e9e8049fbb8f5
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2019-11-27 19:03:38 +0100

    loading from array

--- languages/english.js
+++
@@ -1,3 +0,0 @@
-{
-  "nature" = ["monkey", "dog", "horse"];
-}
--- loading.js
+++ loading.js
@@ -1,6 +1,18 @@
 const text_element = document.querySelector(".theWord--text");
+let length;
 
 fetch("./languages/english.json")
-  .then(e => e.json())
-  .then(e => console.log(e.animals))
+  .then(data => data.json())
+  .then(data => {
+    length = data.length;
+    return data;
+  })
+  .then(data => {
+    let randomTopic = Math.floor(Math.random() * length);
+    return data[randomTopic];
+  })
+  .then(data => {
+    text_element.innerHTML = data;
+    return data;
+  })
   .catch(err => console.log(err));
